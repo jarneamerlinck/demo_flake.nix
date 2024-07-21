409658dabe667ca649528dd7d7777806029d1974
jarneamerlinck <jarneamerlinck@pm.me>
2024-07-21 17:08:22 +0200

    try more

--- disk-config.nix
+++ disk-config.nix
@@ -32,17 +32,17 @@
                     mountpoint = "/";
                   };
                   # Subvolume name is the same as the mountpoint
-                  "/home" = {
-                    mountOptions = [ "compress=zstd" ];
-                    mountpoint = "/home";
-                  };
-                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
-                  # "/home/elliott" = { };
-                  # Parent is not mounted so the mountpoint must be set
-                  "/nix" = {
-                    mountOptions = [ "compress=zstd" "noatime" ];
-                    mountpoint = "/nix";
-                  };
+                  # "/home" = {
+                  #   mountOptions = [ "compress=zstd" ];
+                  #   mountpoint = "/home";
+                  # };
+                  # # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+                  # # "/home/elliott" = { };
+                  # # Parent is not mounted so the mountpoint must be set
+                  # "/nix" = {
+                  #   mountOptions = [ "compress=zstd" "noatime" ];
+                  #   mountpoint = "/nix";
+                  # };
                 };
 
                 mountpoint = "/partition-root";
