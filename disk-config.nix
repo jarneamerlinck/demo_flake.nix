370857c44f5fa786b700b54bc9ffcc726db58363
jarneamerlinck <jarneamerlinck@pm.me>
2024-07-21 17:01:44 +0200

    more tries

--- disk-config.nix
+++ disk-config.nix
@@ -1,9 +1,9 @@
 {
   disko.devices = {
     disk = {
-      main = {
+      vdb = {
         type = "disk";
-        device = "/dev/disk/by-diskseq/1";
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
@@ -24,8 +24,28 @@
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ]; # Override existing partition
-                mountpoint = "/";
-                # mountOptions = [ "compress=zstd" "noatime" ];
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  # Subvolume name is different from mountpoint
+                  "/rootfs" = {
+                    mountpoint = "/";
+                  };
+                  # Subvolume name is the same as the mountpoint
+                  "/home" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
+                  # "/home/elliott" = { };
+                  # Parent is not mounted so the mountpoint must be set
+                  "/nix" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
+                  };
+                };
+
+                mountpoint = "/partition-root";
               };
             };
           };
