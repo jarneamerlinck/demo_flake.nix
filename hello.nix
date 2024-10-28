b1551d681eada22b30bcb3692a13ad89ce5c47d4
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-28 18:58:31 +0100

    added lock and hash

--- hello.nix
+++ hello.nix
@@ -16,7 +16,7 @@
   mkDerivation = {
     src = builtins.fetchTarball {
       url = "https://ftp.gnu.org/gnu/hello/hello-${config.version}.tar.gz";
-      sha256 = "sha256-jZkUKv2SV28wsM18tCqNxoCZmLxdYH2Idh9RLibH2yA=";
+      sha256 = "sha256:0xw6cr5jgi1ir13q6apvrivwmmpr5j8vbymp0x6ll0kcv6366hnn";
     };
   };
 }
