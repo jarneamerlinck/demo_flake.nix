405120b39209bd6e4208e7afc104db25304950ba
jarneamerlinck <jarneamerlinck@pm.me>
2024-10-28 19:04:47 +0100

    add demo ML flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
-  description = "A flake for my dream2nix packages";
+  # This example flake.nix is pretty generic and the same for all
+  # examples, except when they define devShells or extra packages.
+  description = "Dream2nix example flake";
 
-  inputs = { #
+  # We import the latest commit of dream2nix main branch and instruct nix to
+  # re-use the nixpkgs revision referenced by dream2nix.
+  # This is what we test in CI with, but you can generally refer to any
+  # recent nixpkgs commit here.
+  inputs = {
     dream2nix.url = "github:nix-community/dream2nix";
     nixpkgs.follows = "dream2nix/nixpkgs";
   };
@@ -10,9 +16,10 @@
     self,
     dream2nix,
     nixpkgs,
-  }:
-  let
-    eachSystem = nixpkgs.lib.genAttrs [ #
+  }: let
+    # A helper that helps us define the attributes below for
+    # all systems we care about.
+    eachSystem = nixpkgs.lib.genAttrs [
       "aarch64-darwin"
       "aarch64-linux"
       "x86_64-darwin"
@@ -20,18 +27,24 @@
     ];
   in {
     packages = eachSystem (system: {
-      hello = dream2nix.lib.evalModules { #
-        packageSets.nixpkgs = nixpkgs.legacyPackages.${system}; #
+      # For each system, we define our default package
+      # by passing in our desired nixpkgs revision plus
+      # any dream2nix modules needed by it.
+      default = dream2nix.lib.evalModules {
+        packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
         modules = [
-          ./hello.nix #
-          { #
+          # Import our actual package definiton as a dream2nix module from ./default.nix
+          ./default.nix
+          {
+            # Aid dream2nix to find the project root. This setup should also works for mono
+            # repos. If you only have a single project, the defaults should be good enough.
             paths.projectRoot = ./.;
+            # can be changed to ".git" or "flake.nix" to get rid of .project-root
             paths.projectRootFile = "flake.nix";
             paths.package = ./.;
           }
         ];
       };
-      default = self.packages.${system}.hello;  #
     });
   };
 }
