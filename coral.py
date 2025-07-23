8b9c743c4f4e910840e2f9005519e3f52eaad8b5
jarneamerlinck <jarneamerlinck@pm.me>
2025-07-23 22:16:41 +0200

    added models

--- coral.py
+++ coral.py
@@ -215,20 +215,53 @@ def draw_objects(draw, objs, labels):
 def main():
   parser = argparse.ArgumentParser(
       formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('-m', '--model', required=False,
-                      help='File path of .tflite file')
-  parser.add_argument('-i', '--input', required=False,
-                      help='File path of image to process')
-  parser.add_argument('-l', '--labels', help='File path of labels file')
+  parser.add_argument('-m', '--model', required=True,
+                      help='File path of .tflite file.')
+  parser.add_argument('-i', '--input', required=True,
+                      help='File path of image to process.')
+  parser.add_argument('-l', '--labels',
+                      help='File path of labels file.')
   parser.add_argument('-t', '--threshold', type=float, default=0.4,
-                      help='Score threshold for detected objects')
+                      help='Score threshold for detected objects.')
   parser.add_argument('-o', '--output',
                       help='File path for the result image with annotations')
   parser.add_argument('-c', '--count', type=int, default=5,
                       help='Number of times to run inference')
   args = parser.parse_args()
-  print(args)
 
+  labels = load_labels(args.labels) if args.labels else {}
+  interpreter = make_interpreter(args.model)
+  interpreter.allocate_tensors()
+
+  image = Image.open(args.input)
+  scale = detect.set_input(interpreter, image.size,
+                           lambda size: image.resize(size, Image.ANTIALIAS))
+
+  print('----INFERENCE TIME----')
+  print('Note: The first inference is slow because it includes',
+        'loading the model into Edge TPU memory.')
+  for _ in range(args.count):
+    start = time.perf_counter()
+    interpreter.invoke()
+    inference_time = time.perf_counter() - start
+    objs = detect.get_output(interpreter, args.threshold, scale)
+    print('%.2f ms' % (inference_time * 1000))
+
+  print('-------RESULTS--------')
+  if not objs:
+    print('No objects detected')
+
+  for obj in objs:
+    print(labels.get(obj.id, obj.id))
+    print('  id:    ', obj.id)
+    print('  score: ', obj.score)
+    print('  bbox:  ', obj.bbox)
+
+  if args.output:
+    image = image.convert('RGB')
+    draw_objects(ImageDraw.Draw(image), objs, labels)
+    image.save(args.output)
+    image.show()
 
 
 if __name__ == '__main__':
